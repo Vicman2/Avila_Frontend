a025130483a29d40c6c70b0d3c3892d367a381ca
Vicman2 <vicmanthebest@gmail.com>
2020-07-08 14:58:10 +0000

    Ready to start

--- src/index.js
+++ src/index.js
@@ -4,11 +4,26 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import {BrowserRouter, withRouter} from 'react-router-dom'
+import {createStore, compose} from 'redux'
+import {Provider} from 'react-redux'
+
+
+
+const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+// const store = createStore(
+//     reducer, 
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
+const app = (
+    // <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    // </Provider>
+)
+
+ReactDOM.render(app,document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change

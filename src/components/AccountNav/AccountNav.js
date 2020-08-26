734db41d9ffd513d630c1e64f8e362a15e406f72
Vicman2 <vicmanthebest@gmail.com>
2020-08-26 10:38:46 +0000

    Added the logout logic

--- src/components/AccountNav/AccountNav.js
+++ src/components/AccountNav/AccountNav.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {flowRight as compose} from 'lodash'
+import * as userActions from '../../store/actions/userActions'
 import './AccountNav.css'
 
 const AccountNav = (props) => {
+    const onLogOut = () => {
+        localStorage.removeItem("token");
+        props.logOut()
+    }
     return(
         <div className="AccountNav">
             <div className="AccountNav_Item">
@@ -16,7 +23,7 @@ const AccountNav = (props) => {
                 <ion-icon name="briefcase-outline"></ion-icon>
                 <p>My Orders</p>
             </div>
-            <div className="AccountNav_Item">
+            <div className="AccountNav_Item" onClick={onLogOut}>
                 <ion-icon name="power-outline"></ion-icon>
                 <p>Logout</p>
             </div>
@@ -24,5 +31,13 @@ const AccountNav = (props) => {
     )
 }
 
+const dispatchMappedToProps = dispatch => {
+    return {
+        logOut : () => dispatch(userActions.logOut())
+    }
+}
 
-export default AccountNav
+
+export default compose(
+    connect(null, dispatchMappedToProps)
+) (AccountNav)
